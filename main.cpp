eb087c263c055eb8ddeacb5de9378646bed409ea
Remyuu <zaomiaoe@outlook.com>
2023-09-04 01:00:46 +0800

    2.2 barycentric coordinates

--- main.cpp
+++ main.cpp
@@ -14,8 +14,8 @@ const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 const TGAColor blue = TGAColor(0, 0, 255, 255);
 
-const int screenWidth  = 51;
-const int screenHeight = 51;
+const int screenWidth  = 250;
+const int screenHeight = 250;
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     int error2 = 0;
@@ -62,17 +62,79 @@ void triangleLine(Vec2i v0, Vec2i v1, Vec2i v2, TGAImage &image, TGAColor color)
     line(v1.u, v1.v, v2.u, v2.v, image, color);
 }
 
-void triangleRaster(Vec2i v0, Vec2i v1, Vec2i v2, TGAImage &image, TGAColor color){
-    // TODO: 绘制一个实心的三角形
+// 2.1.3 的方法 - 光栅化三角形
+void triangleRaster_2_1_3(Vec2i v0, Vec2i v1, Vec2i v2, TGAImage &image, TGAColor color) {
+    if (v0.v > v1.v) std::swap(v0, v1);
+    if (v0.v > v2.v) std::swap(v0, v2);
+    if (v1.v > v2.v) std::swap(v1, v2);
 
+    // Helper function to compute the intersection of the line and a scanline
+    auto interpolate = [](int y, Vec2i v1, Vec2i v2) -> int {
+        if (v1.v == v2.v) return v1.u;
+        return v1.u + (v2.u - v1.u) * (y - v1.v) / (v2.v - v1.v);
+    };
+
+    for (int y = v0.v; y <= v2.v; y++) {
+        // Intersect triangle sides with scanline
+        int xa = interpolate(y, v0, v2); // Intersection with line v0-v2
+        int xb = (y < v1.v) ? interpolate(y, v0, v1) : interpolate(y, v1, v2); // Depending on current half
+
+        if (xa > xb) std::swap(xa, xb);
+
+        // Draw horizontal line
+        for (int x = xa; x <= xb; x++) {
+            image.set(x, y, color);
+        }
+    }
 }
+
+Vec3f barycentric(Vec2i v0, Vec2i v1, Vec2i v2, Vec2i pixel){
+    // v0, v1, v2 correspond to ABC
+    Vec3f u = Vec3f(v1.x-v0.x,// AB_x
+                    v2.x-v0.x,// AC_x
+                    v0.x-pixel.x)// PA_x
+              ^
+              Vec3f(v1.y-v0.y,
+                    v2.y-v0.y,
+                    v0.y-pixel.y);
+    if (std::abs(u.z)<1) return Vec3f(-1,1,1);
+    return Vec3f(1.f-(u.x+u.y)/u.z, u.y/u.z, u.x/u.z);
+}
+// 重心坐标的方法 - 光栅化三角形
+void triangleRaster(Vec2i v0, Vec2i v1, Vec2i v2, TGAImage &image, TGAColor color){
+    // Find The Bounding Box
+    Vec2i* pts[] = {&v0, &v1, &v2};// Pack
+    Vec2i boundingBoxMin(image.get_width()-1,  image.get_height()-1);
+    Vec2i boundingBoxMax(0, 0);
+    Vec2i clamp(image.get_width()-1, image.get_height()-1);
+    for (int i=0; i<3; i++) {
+        boundingBoxMin.x = std::max(0, std::min(boundingBoxMin.x, pts[i]->x));
+        boundingBoxMin.y = std::max(0, std::min(boundingBoxMin.y, pts[i]->y));
+
+        boundingBoxMax.x = std::min(clamp.x, std::max(boundingBoxMax.x, pts[i]->x));
+        boundingBoxMax.y = std::min(clamp.y, std::max(boundingBoxMax.y, pts[i]->y));
+    }
+
+    // For Loop To Iterate Over All Pixels Within The Bounding Box
+    Vec2i pixel;
+    for (pixel.x = boundingBoxMin.x; pixel.x <= boundingBoxMax.x; pixel.x++) {
+        for (pixel.y = boundingBoxMin.y; pixel.y <= boundingBoxMax.y; pixel.y++) {
+            Vec3f bc = barycentric(v0, v1, v2, pixel);
+            if (bc.x<0 || bc.y<0 || bc.z<0 ) continue;
+            image.set(pixel.x, pixel.y, color);
+        }
+    }
+}
+
 
 int main(int argc, char** argv) {
     TGAImage image(screenWidth, screenHeight, TGAImage::RGB);
 
 //    triangleLine(Vec2i(0,0),Vec2i(25,25),Vec2i(50,16),image,red);
 
-    triangleRaster(Vec2i(0,0),Vec2i(25,25),Vec2i(50,0),image,red);
+//    triangleRaster_2_1_3(Vec2i(10,10), Vec2i(100, 30), Vec2i(190, 160),image,red);
+
+    triangleRaster(Vec2i(10,10), Vec2i(100, 30), Vec2i(190, 160),image,red);
 
     image.flip_vertically();
     image.write_tga_file("output.tga");
