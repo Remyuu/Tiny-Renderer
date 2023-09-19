b0392961b59f3710728c5c9f4906dfd2c4addbf7
Remyuu <zaomiaoe@outlook.com>
2023-09-19 14:40:11 +0800

    6.1 Our_GL

--- main.cpp
+++ main.cpp
@@ -1,158 +1,66 @@
 #include <vector>
-#include <cmath>
-#include <limits>
+#include <iostream>
+
 #include "tgaimage.h"
 #include "model.h"
 #include "geometry.h"
+#include "our_gl.h"
 
+Model *model     = NULL;
 const int width  = 800;
 const int height = 800;
-const int depth  = 255;
 
-Model *model = NULL;
-int *zBuffer = NULL;
-Vec3f light_dir(0,0,-1);
-Vec3f camera(0,0,3);
+Vec3f light_dir(1,1,1);
+Vec3f       eye(0,-1,3);
+Vec3f    center(0,0,0);
+Vec3f        up(0,1,0);
 
-// 4d-->3d
-Vec3f m2v(Matrix m) {
-    return Vec3f(m[0][0]/m[3][0], m[1][0]/m[3][0], m[2][0]/m[3][0]);
-}
+struct GouraudShader : public IShader {
+    Vec3f varying_intensity; // written by vertex shader, read by fragment shader
 
-// 3d-->4d
-Matrix v2m(Vec3f v) {
-    Matrix m(4, 1);
-    m[0][0] = v.x;
-    m[1][0] = v.y;
-    m[2][0] = v.z;
-    m[3][0] = 1.f;
-    return m;
-}
-
-// 视角矩阵
-Matrix viewport(int x, int y, int w, int h) {
-    Matrix m = Matrix::identity(4);
-    m[0][3] = x+w/2.f;
-    m[1][3] = y+h/2.f;
-    m[2][3] = depth/2.f;
-
-    m[0][0] = w/2.f;
-    m[1][1] = h/2.f;
-    m[2][2] = depth/2.f;
-    return m;
-}
-
-Vec3f barycentric(Vec3i *pts, Vec3i P) {
-    Vec3f u =
-            Vec3f(pts[2].x-pts[0].x, pts[1].x-pts[0].x, pts[0].x-P.x)^
-            Vec3f(pts[2].y-pts[0].y, pts[1].y-pts[0].y, pts[0].y-P.y)
-    ;
-    if (std::abs(u.z)<1) return Vec3f(-1,1,1);
-    return Vec3f(1.f-(u.x+u.y)/u.z, u.y/u.z, u.x/u.z);
-}
-void triangle(Vec3i t0, Vec3i t1, Vec3i t2,
-              Vec2i uv0, Vec2i uv1, Vec2i uv2,
-              TGAImage &image, float intensity, int *zbuffer) {
-    // Compute bounding box of the triangle
-    Vec3i bboxmin(image.get_width() - 1, image.get_height() - 1, 0);
-    Vec3i bboxmax(0, 0, 0);
-    Vec3i clamp(image.get_width() - 1, image.get_height() - 1, 0);
-    bboxmin.x = std::max(0, std::min(bboxmin.x, std::min(t0.x, std::min(t1.x, t2.x))));
-    bboxmin.y = std::max(0, std::min(bboxmin.y, std::min(t0.y, std::min(t1.y, t2.y))));
-    bboxmax.x = std::min(clamp.x, std::max(t0.x, std::max(t1.x, t2.x)));
-    bboxmax.y = std::min(clamp.y, std::max(t0.y, std::max(t1.y, t2.y)));
-    Vec3i pts[3] = {t0, t1, t2};
-    Vec3i P;
-    for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++) {
-        for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++) {
-            Vec3f bc_screen = barycentric(pts, P);
-            if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) continue;  // P is outside of triangle
-            P.z = bc_screen.x * t0.z + bc_screen.y * t1.z + bc_screen.z * t2.z;
-            Vec2i uvP;
-            uvP.x = bc_screen.x * uv0.x + bc_screen.y * uv1.x + bc_screen.z * uv2.x;
-            uvP.y = bc_screen.x * uv0.y + bc_screen.y * uv1.y + bc_screen.z * uv2.y;
-            int idx = P.x + P.y * image.get_width();
-            if (zbuffer[idx] < P.z) {
-                zbuffer[idx] = P.z;
-                TGAColor color = model->diffuse(uvP);
-                image.set(P.x, P.y,
-                          TGAColor(color.r * intensity, color.g * intensity, color.b * intensity));
-            }
-        }
+    virtual Vec4f vertex(int iface, int nthvert) {
+        Vec4f gl_Vertex = embed<4>(model->vert(iface, nthvert)); // read the vertex from .obj file
+        gl_Vertex = Viewport*Projection*ModelView*gl_Vertex;     // transform it to screen coordinates
+        varying_intensity[nthvert] = std::max(0.f, model->normal(iface, nthvert)*light_dir); // get diffuse lighting intensity
+        return gl_Vertex;
     }
-}
 
-Matrix lookAt(Vec3f cameraPos, Vec3f lookAt, Vec3f viewUp){
-    //计算出z，根据z和up算出x，再算出y
-    Vec3f gazeDirection = (cameraPos - lookAt).normalize(); // -z 轴
-    Vec3f horizon = (viewUp ^ gazeDirection).normalize(); // 与相机水平的轴
-    Vec3f vertical = (gazeDirection ^ horizon).normalize(); //
-
-    Matrix rotation = Matrix::identity(4);
-    Matrix translation = Matrix::identity(4);
-    for (int i = 0; i < 3; i++) {
-        rotation[i][3] = - lookAt[i];
-        rotation[0][i] = horizon[i];
-        rotation[1][i] = vertical[i];
-        rotation[2][i] = gazeDirection[i];
+    virtual bool fragment(Vec3f bar, TGAColor &color) {
+        float intensity = varying_intensity*bar;   // interpolate intensity for the current pixel
+        color = TGAColor(255, 255, 255)*intensity; // well duh
+        return false;                              // no, we do not discard this pixel
     }
-    //这样乘法的效果是先平移物体，再旋转
-    Matrix res = rotation*translation;
-    return res;
-}
-
+};
 
 int main(int argc, char** argv) {
-    model = new Model("../object/african_head.obj");
-    zBuffer = new int[width*height];
+//    if (2==argc) {
+//        model = new Model(argv[1]);
+//    } else {
+        model = new Model("../object/african_head.obj");
+//    }
 
-    for (int i=width*height; i--; zBuffer[i] = -std::numeric_limits<float>::max());
+    lookat(eye, center, up);
+    viewport(width/8, height/8, width*3/4, height*3/4);
+    projection(-1.f/(eye-center).norm());
+    light_dir.normalize();
 
-    { // draw the model
-        Matrix ModelView  = lookAt(Vec3f(2,1,3), Vec3f(0,0,1), Vec3f(0,1,0));
-        Matrix Projection = Matrix::identity(4);
-        Matrix ViewPort   = viewport(width/8, height/8, width*3/4, height*3/4);
-        Projection[3][2] = -1.f/camera.z;
+    TGAImage image  (width, height, TGAImage::RGB);
+    TGAImage zbuffer(width, height, TGAImage::GRAYSCALE);
 
-        TGAImage image(width, height, TGAImage::RGB);
-        for (int i=0; i<model->nfaces(); i++) {
-            std::vector<int> face = model->face(i);
-            Vec3i screen_coords[3];
-            Vec3f world_coords[3];
-            for (int j=0; j<3; j++) {
-                Vec3f v = model->vert(face[j]);
-                // 视角矩阵*投影矩阵*坐标
-                screen_coords[j] =  m2v(ViewPort*Projection*ModelView*v2m(v));
-                world_coords[j]  = v;
-            }
-            // 计算法向量并且标准化
-            Vec3f n = ((world_coords[2]-world_coords[0])^(world_coords[1]-world_coords[0])).normalize();
-            // 计算光照
-            float intensity = n*light_dir;
-            if (intensity>0) {
-                Vec2i uv[3];
-                for (int k=0; k<3; k++) {
-                    uv[k] = model->uv(i, k);
-                }
-                // 绘制三角形
-                triangle(screen_coords[0], screen_coords[1], screen_coords[2],
-                         uv[0], uv[1], uv[2], image, intensity, zBuffer);
-            }
+    GouraudShader shader;
+    for (int i=0; i<model->nfaces(); i++) {
+        Vec4f screen_coords[3];
+        for (int j=0; j<3; j++) {
+            screen_coords[j] = shader.vertex(i, j);
         }
-        image.flip_vertically();
-        image.write_tga_file("output.tga");
+        triangle(screen_coords, shader, image, zbuffer);
     }
-    { // 输出zbuffer
-        TGAImage zbimage(width, height, TGAImage::GRAYSCALE);
-        for (int i=0; i<width; i++) {
-            for (int j=0; j<height; j++) {
-                zbimage.set(i, j, TGAColor(zBuffer[i+j*width], 1));
-            }
-        }
-        zbimage.flip_vertically();
-        zbimage.write_tga_file("zbuffer.tga");
-    }
+
+    image.  flip_vertically(); // to place the origin in the bottom left corner of the image
+    zbuffer.flip_vertically();
+    image.  write_tga_file("output.tga");
+    zbuffer.write_tga_file("zbuffer.tga");
+
     delete model;
-    delete [] zBuffer;
     return 0;
 }
--- model.h
+++ model.h
@@ -1,28 +1,33 @@
 #ifndef __MODEL_H__
 #define __MODEL_H__
-
 #include <vector>
+#include <string>
 #include "geometry.h"
 #include "tgaimage.h"
 
 class Model {
 private:
-    std::vector<Vec3f> verts_; // 模型的顶点
-    std::vector<std::vector<Vec3i> > faces_; // this Vec3i means vertex/uv/normal
-    std::vector<Vec3f> norms_; // 存储模型的法线
-    std::vector<Vec2f> uv_; // 存储模型的 UV 纹理坐标
-    TGAImage diffusemap_; // 模型的漫反射纹理图像
-    // load_texture() 在加载模型的时候会用到，用于加载纹理。
+    std::vector<Vec3f> verts_;
+    std::vector<std::vector<Vec3i> > faces_; // attention, this Vec3i means vertex/uv/normal
+    std::vector<Vec3f> norms_;
+    std::vector<Vec2f> uv_;
+    TGAImage diffusemap_;
+    TGAImage normalmap_;
+    TGAImage specularmap_;
     void load_texture(std::string filename, const char *suffix, TGAImage &img);
 public:
-    Model(const char *filename); // 构造函数，从给定文件名加载模型
-    ~Model(); // 析构函数，用于释放模型所占用的资源
-    int nverts(); // 返回模型的顶点数量
-    int nfaces(); // 返回模型的面数量
-    Vec3f vert(int i); // 返回指定索引的顶点
-    Vec2i uv(int iface, int nvert); // 返回指定面和指定顶点的 UV 坐标
-    TGAColor diffuse(Vec2i uv); // 根据给定的 UV 坐标，从纹理图中获取颜色
-    std::vector<int> face(int idx); // 返回指定索引的面信息（可能是顶点/纹理坐标/法线的索引）
+    Model(const char *filename);
+    ~Model();
+    int nverts();
+    int nfaces();
+    Vec3f normal(int iface, int nthvert);
+    Vec3f normal(Vec2f uv);
+    Vec3f vert(int i);
+    Vec3f vert(int iface, int nthvert);
+    Vec2f uv(int iface, int nthvert);
+    TGAColor diffuse(Vec2f uv);
+    float specular(Vec2f uv);
+    std::vector<int> face(int idx);
 };
-
 #endif //__MODEL_H__
+
